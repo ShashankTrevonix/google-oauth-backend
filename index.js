8ab7ee62c4f634ed0cf7926d19aea9089cd9c118
ShashankTrevonix <shashanka@trevonix.com>
2025-05-09 20:04:38 +0530

    send

--- index.js
+++ index.js
@@ -12,23 +12,23 @@ app.get('/oauth2redirect', async (req, res) => {
   console.log(`code: ${code}`)
   const redirectToApp = `com.example.google_signin_app_1://oauth2redirect?code=${code}`;
 
-  res.redirect(redirectToApp);
-  // res.send(`
-  //   <!DOCTYPE html>
-  //   <html>
-  //     <head>
-  //       <title>Redirecting...</title>
-  //       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  //       <script type="text/javascript">
-  //         // Trigger Android app deep link
-  //         window.location.href = "${redirectToApp}";
-  //       </script>
-  //     </head>
-  //     <body>
-  //       <p>Redirecting to the app...</p>
-  //     </body>
-  //   </html>
-  // `);
+  // res.redirect(redirectToApp);
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Redirecting...</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script type="text/javascript">
+          // Trigger Android app deep link
+          window.location.href = "${redirectToApp}";
+        </script>
+      </head>
+      <body>
+        <p>Redirecting to the app...</p>
+      </body>
+    </html>
+  `);
 });
 
 const PORT = process.env.PORT || 3000;
