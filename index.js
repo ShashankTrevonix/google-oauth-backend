bd31e0e937f6c9ba055819be58be4a3e382f981e
ShashankTrevonix <shashanka@trevonix.com>
2025-05-09 23:45:10 +0530

    hi

--- index.js
+++ index.js
@@ -10,26 +10,11 @@ app.get('/oauth2redirect', async (req, res) => {
   console.log("Calling oauth2redirect")
   const code = req.query.code;
   console.log(`code: ${code}`)
-  const redirectToApp = 'com.example.google_signin_app_1://oauth2redirect?code='+code;
+  const redirectToApp = 'com.example.googlesigninapp1://oauth2redirect?code='+code;
 
-  // res.redirect(redirectToApp);
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta http-equiv="refresh" content="0; url=${redirectToApp}">
-        <title>Redirecting...</title>
-      </head>
-      <body>
-        <p>Redirecting to the app...<a href="${redirectToApp}">Click here</a></p>
-      </body>
-    </html>
-  `);
+  res.redirect(redirectToApp);
 });
 
-app.get(`/com.example.google_signin_app_1://oauth2redirect`, (req, res) => {
-  res.json({message: "Saav marae"});
-})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
