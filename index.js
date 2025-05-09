6ea87df1a288df0039870055043b32f0db9083e7
ShashankTrevonix <shashanka@trevonix.com>
2025-05-09 19:04:06 +0530

    changes redirect response

--- index.js
+++ index.js
@@ -11,7 +11,23 @@ app.get('/oauth2redirect', async (req, res) => {
   const code = req.query.code;
   console.log(`code: ${code}`)
   const redirectToApp = `com.example.google_signin_app_1:/oauth2redirect?code=${code}`;
-  res.redirect(redirectToApp);
+ 
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Redirecting...</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script type="text/javascript">
+          // Trigger Android app deep link
+          window.location.href = "${redirectToApp}";
+        </script>
+      </head>
+      <body>
+        <p>Redirecting to the app...</p>
+      </body>
+    </html>
+  `);
 });
 
 const PORT = process.env.PORT || 3000;
